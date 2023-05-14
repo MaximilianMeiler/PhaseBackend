ea302a528e5d67ae78f65c59091b669676cad4b7
MaximilianMeiler <maximilianmeiler@ufl.edu>
2023-05-14 08:51:15 -0400

    vercel 1

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
  console.log(`Example app listening on port ${port}`)
 })
 
