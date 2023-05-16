77f0d4216b8b525af56196bdb1dae40fb906ac6b
MaximilianMeiler <maximilianmeiler@ufl.edu>
2023-05-16 10:49:49 -0400

    get user by db id

--- routes/users.mjs
+++ routes/users.mjs
@@ -12,6 +12,15 @@ router.get("/", async (req, res) => {
   res.send(results).status(200);
 });
 
+router.get("/object/:id", async (req, res) => {
+  let collection = await db.collection("users");
+  let query = {_id: new ObjectId(req.params.id)};
+  let result = await collection.findOne(query);
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
 router.get("/:id", async (req, res) => {
   let collection = await db.collection("users");
   let query = {"data.deviceID": req.params.id};
