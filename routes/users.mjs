1e8cc1c140e990087879adce8645d67eb3d2671d
MaximilianMeiler <maximilianmeiler@ufl.edu>
2023-05-17 10:22:30 -0400

    add/remove friends/requests

--- routes/users.mjs
+++ routes/users.mjs
@@ -12,6 +12,78 @@ router.get("/", async (req, res) => {
   res.send(results).status(200);
 });
 
+router.patch("/friendadd/:id1/:id2", async (req, res) => {
+  let collection = await db.collection("users");
+  let user = {_id: new ObjectId(req.params.id1)};
+  let result = await collection.findOne(user);
+
+  if (!result) res.send("Not found").status(404);
+  else {
+    const newFriends = result.data.friends.concat([req.params.id2])
+    const update = {
+      $set: {
+        "data.friends": newFriends
+      }
+    }
+    let updatedResult = await collection.updateOne(user, update);
+    res.send(updatedResult).status(200);
+  }
+});
+
+router.patch("/reqadd/:id1/:id2", async (req, res) => {
+  let collection = await db.collection("users");
+  let user = {_id: new ObjectId(req.params.id1)};
+  let result = await collection.findOne(user);
+
+  if (!result) res.send("Not found").status(404);
+  else {
+    const newReqs = result.data.requests.concat([req.params.id2])
+    const update = {
+      $set: {
+        "data.requests": newReqs
+      }
+    }
+    let updatedResult = await collection.updateOne(user, update);
+    res.send(updatedResult).status(200);
+  }
+});
+
+router.patch("/frienddel/:id1/:id2", async (req, res) => {
+  let collection = await db.collection("users");
+  let user = {_id: new ObjectId(req.params.id1)};
+  let result = await collection.findOne(user);
+
+  if (!result) res.send("Not found").status(404);
+  else {
+    const newFriends = result.data.friends.filter(f => f != req.params.id2)
+    const update = {
+      $set: {
+        "data.friends": newFriends
+      }
+    }
+    let updatedResult = await collection.updateOne(user, update);
+    res.send(updatedResult).status(200);
+  }
+});
+
+router.patch("/reqdel/:id1/:id2", async (req, res) => {
+  let collection = await db.collection("users");
+  let user = {_id: new ObjectId(req.params.id1)};
+  let result = await collection.findOne(user);
+
+  if (!result) res.send("Not found").status(404);
+  else {
+    const newReqs = result.data.requests.filter(r => r != req.params.id2)
+    const update = {
+      $set: {
+        "data.requests": newReqs
+      }
+    }
+    let updatedResult = await collection.updateOne(user, update);
+    res.send(updatedResult).status(200);
+  }
+});
+
 router.get("/object/:id", async (req, res) => {
   let collection = await db.collection("users");
   let query = {_id: new ObjectId(req.params.id)};
